f3ded55573c679e44714b675817b3a2865554f8d
Michael Daryl Mayo <michaeldaryl.code@gmail.com>
2018-08-11 22:42:12 +0800

    Registered models to admin site

--- candidates/admin.py
+++ candidates/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
+from candidates.models import *
 
-# Register your models here.
+
+admin.site.register(Candidate)
+admin.site.register(PrePageant)
+admin.site.register(OldStreetFashionAttire)
+admin.site.register(UniformAttire)
+admin.site.register(FormalAttire)
+admin.site.register(QuestionAndAnswer)
+admin.site.register(PrePageantTotal)
+admin.site.register(OldStreetFashionAttireTotal)
+admin.site.register(UniformAttireTotal)
+admin.site.register(FormalAttireTotal)
+admin.site.register(QuestionAndAnswerTotal)
+admin.site.register(PageantProper)
+admin.site.register(PageantNight)
+admin.site.register(PageantResult)
