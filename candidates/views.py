25222f9a360f5bf6f5e91898495188219522a11f
Michael Daryl Mayo <michaeldaryl.code@gmail.com>
2018-08-11 22:24:02 +0800

    Rendered candidate index template

--- candidates/views.py
+++ candidates/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render
+from candidates.models import *
 
 
 def index(request):
-    pass
+    context = {
+
+    }
+
+    return render(request, 'candidates/index.html', context)
